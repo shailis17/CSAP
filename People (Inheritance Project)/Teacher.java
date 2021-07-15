579b8bd8f4f966ebcf6afb8dcabe13adf74ed3ee
shailis17 <83250817+shailis17@users.noreply.github.com>
2021-07-15 10:22:19 -0400

    Create Teacher.java

---
+++ People (Inheritance Project)/Teacher.java
@@ -0,0 +1,37 @@
+public class Teacher extends Person
+{
+  private String subject;
+  private int yearsExperience;
+  
+  public Teacher(String n, int a, char g, String s, int y)
+  {
+    super(n, a, g);
+    subject = s;
+    yearsExperience = y;
+  }
+  
+  public String getSubject()
+  {
+    return subject;
+  }
+  
+  public int getYears()
+  {
+    return yearsExperience;
+  }
+  
+  public void setSubject(String s)
+  {
+    subject = s;
+  }
+
+  public void setYears(int y)
+  {
+    yearsExperience = y;
+  }
+  
+  public String toString()
+  {
+    return super.toString() + ", Subject: " + subject + ", Years Experience: " + yearsExperience;
+  }
+}
